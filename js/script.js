ed44e2c04d5e3d031ffbfca8d4bf20dbb2c732a8
Pervez Iqbal <iqbalpervez121@gmail.com>
2023-01-23 23:48:32 -0800

    dumped first chunk

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// const artistSec = [...document.querySelectorAll('.artsts-sec')];
+// const backBtn =  [...document.querySelectorAll('#back-btn')];
+// const nextBtn =  [...document.querySelectorAll('#next-btn')];
+
+
+// artistSec.forEach((item, i) => {
+//     let containerDimenion = item.getBoundingClientRect();
+//     let containerWidth = containerDimenion.width;
+
+//     nextBtn[i].addEventListener('click', () => {
+//         item.scrollLeft += containerWidth;
+//     })
+//     backBtn[i].addEventListener('click', () => {
+//         item.scrollLeft -= containerWidth;
+//     })
+
+// })
+new Glider(document.querySelector('.glider'), {
+    slidesToshow: 4,
+    slidesToScroll: 2,
+    arrows:{
+        prev: ".glider-prev",
+        next:".glider-next"
+    }
+});
